92e1e0dc9d320d644bcdb5dd6f70eefbced8622c
hongfs <hong@hongfs.cn>
2023-09-16 18:47:25 +0800

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -8,10 +8,12 @@ class Service extends BaseService
 {
     public function register()
     {
+        // 注册命令
         $this->commands([
             'schema:load' => command\SchemaLoad::class,
         ]);
 
+        // 注册事件
         $this->app->event->bind([
             'HttpRun' => [
                 'Hongfs\ThinkSchema\event\SchemaLoad',
